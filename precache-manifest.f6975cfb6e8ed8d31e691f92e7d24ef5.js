0e1da71da43f263c4972bae7ff271f3fb20ac808
Ben Rose <benrose207@gmail.com>
2020-10-06 17:27:42 -0700

    Updates

---
+++ precache-manifest.f6975cfb6e8ed8d31e691f92e7d24ef5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b960a0edd5b2419b5146847799b03b",
+    "url": "/project-playground/index.html"
+  },
+  {
+    "revision": "bb67d3939fb5c4039183",
+    "url": "/project-playground/static/css/main.f28ff90b.chunk.css"
+  },
+  {
+    "revision": "f9f05f107e609085cacf",
+    "url": "/project-playground/static/js/2.060d152f.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/project-playground/static/js/2.060d152f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb67d3939fb5c4039183",
+    "url": "/project-playground/static/js/main.a5b6428b.chunk.js"
+  },
+  {
+    "revision": "22fed9e6f3680f6c0287",
+    "url": "/project-playground/static/js/runtime-main.a8abf3f1.js"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/project-playground/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/project-playground/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "eae0deb3d6dd859a4732c4328066a38e",
+    "url": "/project-playground/static/media/drum-kit.eae0deb3.jpg"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/project-playground/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/project-playground/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/project-playground/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/project-playground/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/project-playground/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "75f727ec5b83f099bdac79c49edca79a",
+    "url": "/project-playground/static/media/tink.75f727ec.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/project-playground/static/media/tom.572ed39d.wav"
+  }
+]);
