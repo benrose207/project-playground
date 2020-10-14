197e02619e0868380fe82e2bd5544a959d984fcd
Ben Rose <benrose207@gmail.com>
2020-10-14 12:21:06 -0700

    add card for new exercise and update button styling

--- src/components/javascript30/javascript30.jsx
+++ src/components/javascript30/javascript30.jsx
@@ -6,6 +6,7 @@ import day4Img from '../../assets/images/js30_day4.png';
 import day5Img from '../../assets/images/js30_day5.png';
 import day6Img from '../../assets/images/js30_day6.png';
 import day7Img from '../../assets/images/js30_day7.png';
+import day8Img from '../../assets/images/js30_day8.png';
 import '../../assets/stylesheets/javascript30.css';
 import Card from './card';
 
@@ -50,7 +51,7 @@ const JavaScript30 = () => {
         <Card img={day5Img} altText="screenshot of flex image gallery" title="Day 5: Flex Panels Image Gallery" path="/javascript30/5"/>
         <Card img={day6Img} altText="screenshot of ajax typeahead" title="Day 6: AJAX Type Ahead" path="/javascript30/6"/>
         <Card img={day7Img} altText="screenshot of javascript array methods" title="Day 7: Array Cardio Day 2" path="/javascript30/7"/>
-        <Card img={day7Img} altText="screenshot of javascript array methods" title="Day 8: Fun with HTML5 Canvas" path="/javascript30/8"/>
+        <Card img={day8Img} altText="screenshot of HTML5 Canvas" title="Day 8: Fun with HTML5 Canvas" path="/javascript30/8"/>
       </section>
     </div>
   )
