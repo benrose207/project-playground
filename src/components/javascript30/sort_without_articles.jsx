d28613be7b0ab5e73bb98114d31c1edaf1fe5829
Ben Rose <benrose207@gmail.com>
2020-11-24 17:36:11 -0800

    complete js30 day 17 exercise

--- src/components/javascript30/sort_without_articles.jsx
+++ src/components/javascript30/sort_without_articles.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 const SortWithoutArticles = () => {
+
+  useEffect(() => {
+    const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+  
+    function strip(bandName) {
+      return bandName.replace(/^(a |the |an )/i, '').trim();
+    }
+
+    const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+  
+    document.querySelector('#bands').innerHTML = sortedBands.map(band => (
+      `<li>${band}</li>`
+    )).join('');
+  }, []);
+
   return (
     <div className="content-container">
       <h1>Sorting Band Names Without Articles</h1>
+      <ul id="bands"></ul>
     </div>
   );
 };
