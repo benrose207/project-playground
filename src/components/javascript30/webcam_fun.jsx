e529ffcaad3ec40b518c758dc74b5c34fff3ba66
Ben Rose <benrose207@gmail.com>
2020-12-17 14:34:39 -0800

    Set up initial photobooth components for js30

--- src/components/javascript30/webcam_fun.jsx
+++ src/components/javascript30/webcam_fun.jsx
@@ -4,41 +4,89 @@ import snap from '../../assets/sounds/snap.mp3';
 const WebcamFun = () => {
   useEffect(() => {
     const video = document.querySelector('.player');
-    const canvas = document.querySelector('.photo');
+    const canvas = document.getElementById('photo');
     const ctx = canvas.getContext('2d');
-    const strip = document.querySelector('.strip');
-    const snap = document.querySelector('.snap');
+    // const strip = document.querySelector('.strip');
+    // const snap = document.querySelector('.snap');
+
+    function getVideo() {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+          video.srcObject = localMediaStream;
+          video.onloadedmetadata = () => {
+            video.play();
+          }
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+
+    function paintToCanvas() {
+      const width = video.videoWidth;
+      const height = video.videoHeight;
+      canvas.width = width;
+      canvas.height = height;
+
+      return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+
+
+      }, 16);
+    }
+
+    getVideo();
+
+    // video.addEventListener('canplay', paintToCanvas);
   });
+
+  const takePhoto = () => {
+    const snap = document.querySelector('.snap');
+    const canvas = document.getElementById('photo');
+    const strip = document.querySelector('.strip');
+
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'selfie');
+    link.innerHTML = `<img src="${data}" alt="Selfie" />`;
+    strip.insertBefore(link, strip.firstChild);
+  }
 
   return (
     <div className="content-container">
       <h1>Unreal Webcam Fun</h1>
       <div className="photobooth">
-        <div className="controls">
-          <button onClick="takePhoto()">Take Photo</button>
-          {/* <div className="rgb">
-            <label for="rmin">Red Min:</label>
-            <input type="range" min='0' max='255' name="rmin" />
-            <label for="rmax">Red Max:</label>
-            <input type="range" min='0' max='255' name="rmax"/>
+        <div className="photo-controls">
+          <button onClick={takePhoto}>Take Photo</button>
+          <div className="rgb">
+            <div>
+              <label htmlFor="rmin">Red Min:</label>
+              <input type="range" min='0' max='255' id="rmin" />
+              <label htmlFor="rmax">Red Max:</label>
+              <input type="range" min='0' max='255' id="rmax"/>
+            </div>
 
-            <br/>
+            <div>
+              <label htmlFor="gmin">Green Min:</label>
+              <input type="range" min='0' max='255' id="gmin"/>
+              <label htmlFor="gmax">Green Max:</label>
+              <input type="range" min='0' max='255' id="gmax"/>
+            </div>
 
-            <label for="gmin">Green Min:</label>
-            <input type="range" min='0' max='255' name="gmin"/>
-            <label for="gmax">Green Max:</label>
-            <input type="range" min='0' max='255' name="gmax"/>
-
-            <br/>
-
-            <label for="bmin">Blue Min:</label>
-            <input type="range" min='0' max='255' name="bmin"/>
-            <label for="bmax">Blue Max:</label>
-            <input type="range" min='0' max='255' name="bmax"/>
-          </div> */}
+            <div>
+              <label htmlFor="bmin">Blue Min:</label>
+              <input type="range" min='0' max='255' id="bmin"/>
+              <label htmlFor="bmax">Blue Max:</label>
+              <input type="range" min='0' max='255' id="bmax"/>
+            </div>
+          </div>
         </div>
 
-        <canvas className="photo"></canvas>
+        <canvas id="photo"></canvas>
         <video className="player"></video>
         <div className="strip"></div>
       </div>
