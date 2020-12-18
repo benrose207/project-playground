c59eb74abc3d6457175d94cec654475d37d281bd
Ben Rose <benrose207@gmail.com>
2020-12-17 16:24:25 -0800

    Add buttons to change photobooth filters

--- src/components/javascript30/webcam_fun.jsx
+++ src/components/javascript30/webcam_fun.jsx
@@ -2,12 +2,11 @@ import React, { useEffect } from 'react';
 import snap from '../../assets/sounds/snap.mp3';
 
 const WebcamFun = () => {
+
   useEffect(() => {
     const video = document.querySelector('.player');
     const canvas = document.getElementById('photo');
     const ctx = canvas.getContext('2d');
-    // const strip = document.querySelector('.strip');
-    // const snap = document.querySelector('.snap');
 
     function getVideo() {
       navigator.mediaDevices.getUserMedia({ video: true, audio: false })
@@ -23,22 +22,111 @@ const WebcamFun = () => {
     }
 
     function paintToCanvas() {
+      let intervalId;
+      if (intervalId) clearInterval(intervalId);
+
       const width = video.videoWidth;
       const height = video.videoHeight;
       canvas.width = width;
       canvas.height = height;
 
-      return setInterval(() => {
+      intervalId =  setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        ctx.globalAlpha = 1.0;
+        
+        const buttons = document.querySelector('.photo-buttons').children;
+        
+        for (let i = 0; i < buttons.length; i++) {
+          const button = buttons[i];
+          if (button.dataset.status === 'active') {
+            if (button.id === 'redEffect') {
+              pixels = redEffect(pixels);
+              // ctx.globalAlpha = 1.0;
+            } else if (button.id === 'rgbaSplit') {
+              pixels = rgbSplit(pixels);
+              ctx.globalAlpha = 0.1;
+            } else if (button.id === 'greenScreen') {
+              pixels = greenScreen(pixels);
+              // ctx.globalAlpha = 1.0;
+            }
+          }
+        }
 
-
+        ctx.putImageData(pixels, 0, 0);
       }, 16);
+    }
+
+    function redEffect(pixels) {
+      const data = pixels.data;
+      for (let i = 0; i < data.length; i += 4) {
+        data[i] += 100;
+        data[i + 1] -= 50;
+        data[i + 2] *= 0.5;
+      }
+
+      return pixels;
+    }
+
+    function rgbSplit(pixels) {
+      const data = pixels.data;
+      for (let i = 0; i < data.length; i += 4) {
+        data[i - 150] = data[i];
+        data[i + 100] = data[i + 1];
+        data[i - 150] = data[i + 2];
+      }
+
+      return pixels;
+    }
+
+    function greenScreen(pixels) {
+      const levels = {};
+
+      document.querySelectorAll('.rgb input').forEach(input => {
+        levels[input.name] = input.value;
+      });
+
+      for (let i = 0; i < pixels.data.length; i += 4) {
+        const red = pixels.data[i];
+        const green = pixels.data[i + 1];
+        const blue = pixels.data[i + 2];
+
+        if (red >= levels.rmin
+          && green >= levels.gmin
+          && blue >= levels.bmin
+          && red <= levels.rmax
+          && green <= levels.gmax
+          && blue <= levels.bmax) {
+          pixels.data[i + 3] = 0;
+          }
+      }
+
+      return pixels;
     }
 
     getVideo();
 
-    // video.addEventListener('canplay', paintToCanvas);
+    video.addEventListener('canplay', paintToCanvas);
   });
+
+  const handleEffectChange = (e) => {
+    const buttons = document.querySelector('.photo-buttons').children;
+    let status = e.target.dataset.status
+
+    for (let i = 0; i < buttons.length; i++) {
+      const button = buttons[i];
+
+      if (e.target.id !== button.id) {
+        button.dataset.status = '';
+      } else {
+        if (!status) {
+          e.target.dataset.status = 'active';
+        } else {
+          e.target.dataset.status = "";
+        }
+      }
+    }
+  }
 
   const takePhoto = () => {
     const snap = document.querySelector('.snap');
@@ -61,27 +149,32 @@ const WebcamFun = () => {
       <h1>Unreal Webcam Fun</h1>
       <div className="photobooth">
         <div className="photo-controls">
-          <button onClick={takePhoto}>Take Photo</button>
+          <div className="photo-buttons">
+            <button onClick={takePhoto}>Take Photo</button>
+            <button onClick={handleEffectChange} data-status="" id='redEffect'>Red Effect</button>
+            <button onClick={handleEffectChange} data-status="" id='rgbaSplit'>RGBA Split</button>
+            <button onClick={handleEffectChange} data-status="" id='greenScreen'>Green Screen</button>
+          </div>
           <div className="rgb">
             <div>
               <label htmlFor="rmin">Red Min:</label>
-              <input type="range" min='0' max='255' id="rmin" />
+              <input type="range" min='0' max='255' id="rmin" name="rmin" />
               <label htmlFor="rmax">Red Max:</label>
-              <input type="range" min='0' max='255' id="rmax"/>
+              <input type="range" min='0' max='255' id="rmax" name="rmax" />
             </div>
 
             <div>
               <label htmlFor="gmin">Green Min:</label>
-              <input type="range" min='0' max='255' id="gmin"/>
+              <input type="range" min='0' max='255' id="gmin" name="gmin" />
               <label htmlFor="gmax">Green Max:</label>
-              <input type="range" min='0' max='255' id="gmax"/>
+              <input type="range" min='0' max='255' id="gmax" name="gmax" />
             </div>
 
             <div>
               <label htmlFor="bmin">Blue Min:</label>
-              <input type="range" min='0' max='255' id="bmin"/>
+              <input type="range" min='0' max='255' id="bmin" name="bmin" />
               <label htmlFor="bmax">Blue Max:</label>
-              <input type="range" min='0' max='255' id="bmax"/>
+              <input type="range" min='0' max='255' id="bmax" name="bmax" />
             </div>
           </div>
         </div>
