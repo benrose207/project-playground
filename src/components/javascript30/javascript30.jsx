b7dd8d83356996cc69e6c2c3e68fbda5028c1c89
Ben Rose <benrose207@gmail.com>
2020-12-24 11:04:18 -0800

    Add image for component teaser

--- src/components/javascript30/javascript30.jsx
+++ src/components/javascript30/javascript30.jsx
@@ -20,6 +20,7 @@ import day18Img from '../../assets/images/js30_day18.png';
 import day19Img from '../../assets/images/js30_day19.jpeg';
 import day20Img from '../../assets/images/js30_day20.png';
 import day21Img from '../../assets/images/js30_day21.png';
+import day22Img from '../../assets/images/js30_day22.png';
 import '../../assets/stylesheets/javascript30.css';
 import Card from './card';
 
@@ -78,7 +79,7 @@ const JavaScript30 = () => {
         <Card img={day19Img} altText="Selfie taken with webcam" title="Day 19: Unreal Webcam Fun" path="/javascript30/19"/>
         <Card img={day20Img} altText="Screenshot showing result of speech recognition" title="Day 20: Native Speech Recognition" path="/javascript30/20"/>
         <Card img={day21Img} altText="Screenshot of compass and speedometer" title="Day 21: Geolocation Speedometer & Compass" path="/javascript30/21"/>
-        <Card img={day21Img} altText="Screenshot of compass and speedometer" title="Day 22: Follow Along Links" path="/javascript30/22"/>
+        <Card img={day22Img} altText="Screenshot of links and lorem ipsem text" title="Day 22: Follow Along Links" path="/javascript30/22"/>
       </section>
     </div>
   )
