c1f41ed02881c2d8c99cfd9bcd5b713e5818dd15
Ben Rose <benrose207@gmail.com>
2020-12-30 17:58:29 -0800

    Finish basics of JS30 day23 project

--- src/components/javascript30/speech_synthesis.jsx
+++ src/components/javascript30/speech_synthesis.jsx
@@ -8,6 +8,46 @@ const SpeechSynthesis = () => {
     const options = document.querySelectorAll('[type="range"], [name="text"]');
     const speakButton = document.querySelector('#speak');
     const stopButton = document.querySelector('#stop');
+    msg.text = document.querySelector('[name="text"]').value;
+
+    function populateVoices() {
+      voices = this.getVoices();
+      voicesDropdown.innerHTML = voices
+        .filter(voice => voice.lang.includes('en'))
+        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+        .join('');
+    }
+
+    function setVoice() {
+      msg.voice = voices.find(voice => voice.name === this.value);
+      toggle();
+    }
+
+    function toggle(startOver = true) {
+      speechSynthesis.cancel();
+      if (startOver) {
+        speechSynthesis.speak(msg);
+      }
+    }
+
+    function setOption() {
+      msg[this.name] = this.value;
+      toggle();
+    }
+
+    speechSynthesis.addEventListener('voiceschanged', populateVoices);
+    voicesDropdown.addEventListener('change', setVoice);
+    options.forEach(option => option.addEventListener('change', setOption));
+    speakButton.addEventListener('click', toggle);
+    stopButton.addEventListener('click', () => toggle(false));
+    
+    return (() => {
+      speechSynthesis.removeEventListener('voiceschanged', populateVoices);
+      voicesDropdown.removeEventListener('change', setVoice);
+      options.forEach(option => option.removeEventListener('change', setOption));
+      speakButton.removeEventListener('click', toggle);
+      stopButton.removeEventListener('click', () => toggle(false));
+    });
   }, [])
 
   return (
@@ -26,7 +66,7 @@ const SpeechSynthesis = () => {
         <input name="rate" type="range" min="0" max="3" defaultValue="1" step="0.1" id="rate" />
 
         <label htmlFor="pitch">Pitch:</label>
-        <input name="pitch" type="range" min="0" max="2" step="0.1" id="pitch" />
+        <input name="pitch" type="range" min="0" max="2" step="0.1" id="pitch" defaultValue="1"/>
         <textarea name="text" aria-label="textarea" defaultValue="Hello! I love JavaScript" />
         <button id="stop">Stop!</button>
         <button id="speak">Speak</button>
