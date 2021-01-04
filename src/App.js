cfb1c23a2fcfda6d19dde4534f99ce66ce9251a4
Ben Rose <benrose207@gmail.com>
2021-01-04 12:13:21 -0800

    Set up boilerplate code and components for new js30 exercise

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import SpeechRecognition from './components/javascript30/speech_recognition';
 import Geolocation from './components/javascript30/geolocation';
 import FollowAlongLinks from './components/javascript30/follow_along_links';
 import SpeechSynthesis from './components/javascript30/speech_synthesis';
+import StickyNav from './components/javascript30/sticky_nav';
 import Footer from './components/navigation/footer';
 
 
@@ -67,6 +68,7 @@ const App = () => {
           <Route path="/javascript30/21" component={Geolocation}/>
           <Route path="/javascript30/22" component={FollowAlongLinks}/>
           <Route path="/javascript30/23" component={SpeechSynthesis}/>
+          <Route path="/javascript30/24" component={StickyNav}/>
         </Switch>
       </main>
       <Footer />
--- src/components/javascript30/javascript30.jsx
+++ src/components/javascript30/javascript30.jsx
@@ -55,36 +55,37 @@ const JavaScript30 = () => {
   return (
     <div className="content-container-grid">
       <h1>JavaScript30</h1>
-      <p>The below projects are my take on the 30 vanilla JavaScript exercises in the 
+      <p>The below projects are my take on the 30 vanilla JavaScript exercises in the
         <a href="https://javascript30.com/" target="_blank" rel="noopener noreferrer"> JavaScript30 course</a>.
       </p>
       <section className="flex-wrap-container">
-        <Card img={drumKitImg} altText="drum set" title="Day 1: Drum Kit" path="/javascript30/1"/>
-        <Card img={clockImg} altText="css clock" title="Day 2: CSS + JS Clock" path="/javascript30/2"/>
-        <Card img={day3Img} altText="screenshot of filters for image" title="Day 3: Playing with CSS Variables and JS" path="/javascript30/3"/>
-        <Card img={day4Img} altText="screenshot of javascript reduce method" title="Day 4: Array Cardio Day 1" path="/javascript30/4"/>
-        <Card img={day5Img} altText="screenshot of flex image gallery" title="Day 5: Flex Panels Image Gallery" path="/javascript30/5"/>
-        <Card img={day6Img} altText="screenshot of ajax typeahead" title="Day 6: AJAX Type Ahead" path="/javascript30/6"/>
-        <Card img={day7Img} altText="screenshot of javascript array methods" title="Day 7: Array Cardio Day 2" path="/javascript30/7"/>
-        <Card img={day8Img} altText="screenshot of HTML5 Canvas" title="Day 8: Fun with HTML5 Canvas" path="/javascript30/8"/>
-        <Card img={day9Img} altText="screenshot of styling in JS console" title="Day 9: 14 Must-Know Dev Tools Tricks" path="/javascript30/9"/>
-        <Card img={day10Img} altText="screenshot of inbox-style checkboxes" title="Day 10: Hold Shift to Check Multiple Checkboxes" path="/javascript30/10"/>
-        <Card img={day11Img} altText="screenshot of custom video player" title="Day 11: Custom HTML5 Video Player" path="/javascript30/11"/>
-        <Card img={day12Img} altText="screenshot of keyup event handler" title="Day 12: Key Sequence Detection" path="/javascript30/12"/>
-        <Card img={day13Img} altText="image of cloud inset into article text" title="Day 13: Slide in on Scroll" path="/javascript30/13"/>
-        <Card img={day14Img} altText="screenshot of various javascript array copying methods" title="Day 14: Object and Arrays - Reference vs. Copy" path="/javascript30/14"/>
-        <Card img={day15Img} altText="checklist of local tapas foods" title="Day 15: LocalStorage and Event Delegation" path="/javascript30/15"/>
-        <Card img={day16Img} altText="Text with several different shadows" title="Day 16: CSS Text Shadow Mouse Move Effect" path="/javascript30/16"/>
-        <Card img={day17Img} altText="Alphabetical list ordered without articles" title="Day 17: Sorting Band Names Without Articles" path="/javascript30/17"/>
-        <Card img={day18Img} altText="List with times and total time" title="Day 18: Tally String Times with Reduce" path="/javascript30/18"/>
-        <Card img={day19Img} altText="Selfie taken with webcam" title="Day 19: Unreal Webcam Fun" path="/javascript30/19"/>
-        <Card img={day20Img} altText="Screenshot showing result of speech recognition" title="Day 20: Native Speech Recognition" path="/javascript30/20"/>
-        <Card img={day21Img} altText="Screenshot of compass and speedometer" title="Day 21: Geolocation Speedometer & Compass" path="/javascript30/21"/>
-        <Card img={day22Img} altText="Screenshot of links and lorem ipsem text" title="Day 22: Follow Along Links" path="/javascript30/22"/>
-        <Card img={day23Img} altText="Screenshot of speech synthesis app with controls" title="Day 23: Speech Synthesis" path="/javascript30/23"/>
+        <Card img={drumKitImg} altText="drum set" title="Day 1: Drum Kit" path="/javascript30/1" />
+        <Card img={clockImg} altText="css clock" title="Day 2: CSS + JS Clock" path="/javascript30/2" />
+        <Card img={day3Img} altText="screenshot of filters for image" title="Day 3: Playing with CSS Variables and JS" path="/javascript30/3" />
+        <Card img={day4Img} altText="screenshot of javascript reduce method" title="Day 4: Array Cardio Day 1" path="/javascript30/4" />
+        <Card img={day5Img} altText="screenshot of flex image gallery" title="Day 5: Flex Panels Image Gallery" path="/javascript30/5" />
+        <Card img={day6Img} altText="screenshot of ajax typeahead" title="Day 6: AJAX Type Ahead" path="/javascript30/6" />
+        <Card img={day7Img} altText="screenshot of javascript array methods" title="Day 7: Array Cardio Day 2" path="/javascript30/7" />
+        <Card img={day8Img} altText="screenshot of HTML5 Canvas" title="Day 8: Fun with HTML5 Canvas" path="/javascript30/8" />
+        <Card img={day9Img} altText="screenshot of styling in JS console" title="Day 9: 14 Must-Know Dev Tools Tricks" path="/javascript30/9" />
+        <Card img={day10Img} altText="screenshot of inbox-style checkboxes" title="Day 10: Hold Shift to Check Multiple Checkboxes" path="/javascript30/10" />
+        <Card img={day11Img} altText="screenshot of custom video player" title="Day 11: Custom HTML5 Video Player" path="/javascript30/11" />
+        <Card img={day12Img} altText="screenshot of keyup event handler" title="Day 12: Key Sequence Detection" path="/javascript30/12" />
+        <Card img={day13Img} altText="image of cloud inset into article text" title="Day 13: Slide in on Scroll" path="/javascript30/13" />
+        <Card img={day14Img} altText="screenshot of various javascript array copying methods" title="Day 14: Object and Arrays - Reference vs. Copy" path="/javascript30/14" />
+        <Card img={day15Img} altText="checklist of local tapas foods" title="Day 15: LocalStorage and Event Delegation" path="/javascript30/15" />
+        <Card img={day16Img} altText="Text with several different shadows" title="Day 16: CSS Text Shadow Mouse Move Effect" path="/javascript30/16" />
+        <Card img={day17Img} altText="Alphabetical list ordered without articles" title="Day 17: Sorting Band Names Without Articles" path="/javascript30/17" />
+        <Card img={day18Img} altText="List with times and total time" title="Day 18: Tally String Times with Reduce" path="/javascript30/18" />
+        <Card img={day19Img} altText="Selfie taken with webcam" title="Day 19: Unreal Webcam Fun" path="/javascript30/19" />
+        <Card img={day20Img} altText="Screenshot showing result of speech recognition" title="Day 20: Native Speech Recognition" path="/javascript30/20" />
+        <Card img={day21Img} altText="Screenshot of compass and speedometer" title="Day 21: Geolocation Speedometer & Compass" path="/javascript30/21" />
+        <Card img={day22Img} altText="Screenshot of links and lorem ipsem text" title="Day 22: Follow Along Links" path="/javascript30/22" />
+        <Card img={day23Img} altText="Screenshot of speech synthesis app with controls" title="Day 23: Speech Synthesis" path="/javascript30/23" />
+        <Card img={day23Img} altText="Screenshot of speech synthesis app with controls" title="Day 24: Sticky Nav" path="/javascript30/24" />
       </section>
     </div>
-  )
+  );
 };
 
 export default JavaScript30;
