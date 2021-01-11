1513c5be3541b273be01b16ee378bb4d45ff41e8
Ben Rose <benrose207@gmail.com>
2021-01-11 14:49:42 -0800

    Add image for new component preview

--- src/components/javascript30/javascript30.jsx
+++ src/components/javascript30/javascript30.jsx
@@ -23,6 +23,7 @@ import day21Img from '../../assets/images/js30_day21.png';
 import day22Img from '../../assets/images/js30_day22.png';
 import day23Img from '../../assets/images/js30_day23.png';
 import day24Img from '../../assets/images/js30_day24.png';
+import day25Img from '../../assets/images/js30_day25.png';
 import '../../assets/stylesheets/javascript30.css';
 import Card from './card';
 
@@ -84,7 +85,7 @@ const JavaScript30 = () => {
         <Card img={day22Img} altText="Screenshot of links and lorem ipsem text" title="Day 22: Follow Along Links" path="/javascript30/22" />
         <Card img={day23Img} altText="Screenshot of speech synthesis app with controls" title="Day 23: Speech Synthesis" path="/javascript30/23" />
         <Card img={day24Img} altText="Screenshot of navbar" title="Day 24: Sticky Nav" path="/javascript30/24" />
-        <Card img={day24Img} altText="Screenshot of navbar" title="Day 25: Event Capture, Bubbling, etc." path="/javascript30/25" />
+        <Card img={day25Img} altText="Screenshot of component title" title="Day 25: Event Capture, Bubbling, etc." path="/javascript30/25" />
       </section>
     </div>
   );
