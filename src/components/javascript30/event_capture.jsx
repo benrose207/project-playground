1daa0b0ef7e3218c8a58acdef8e5388f90c4126d
Ben Rose <benrose207@gmail.com>
2021-01-11 14:45:25 -0800

    Complete js30 day25 exercise

--- src/components/javascript30/event_capture.jsx
+++ src/components/javascript30/event_capture.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const EventCapture = () => {
+  useEffect(() => {
+    const container = document.querySelector('.event-capture');
+    const divs = container.querySelectorAll('div');
+
+    function logText(e) {
+      console.log(this.classList.value);
+      // e.stopPropagation(); // stop bubbling
+    }
+
+    divs.forEach(div => div.addEventListener('click', logText, {
+      capture: false,
+      once: false // e.g. only allow clicking a button once
+    }));
+
+    return (() => {
+      divs.forEach(div => div.removeEventListener('click', logText));
+    })
+  }, []);
+
   return (
     <div className="content-container event-capture">
       <h1>Event Capture, Propagation, Bubbling</h1>
@@ -11,8 +30,6 @@ const EventCapture = () => {
           </div>
         </div>
       </div>
-
-      <button></button>
     </div>
   );
 };
