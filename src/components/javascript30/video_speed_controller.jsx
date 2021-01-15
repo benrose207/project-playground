d825ca760b9b7b87c406b7648f4e6b265aa2b9c2
Ben Rose <benrose207@gmail.com>
2021-01-14 16:52:10 -0800

    Complete js30 day28 exercise

--- src/components/javascript30/video_speed_controller.jsx
+++ src/components/javascript30/video_speed_controller.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const VideoSpeedController = () => {
+  useEffect(() => {
+    const speed = document.querySelector('.video-speed');
+    const bar = speed.querySelector('.speed-bar');
+    const video = document.querySelector('.flex');
+
+    function handleMove(e) {
+      const y = e.pageY - this.offsetTop;
+      const percent = y / this.offsetHeight;
+      const min = 0.4;
+      const max = 4;
+      const height = Math.round(percent * 100) + '%';
+      const playbackRate = percent * (max - min) + min;
+
+      bar.style.height = height;
+      bar.textContent = playbackRate.toFixed(2) + 'x';
+      video.playbackRate = playbackRate;
+    }
+
+    speed.addEventListener('mousemove', handleMove);
+    
+    return (() => {
+      speed.removeEventListener('mousemove', handleMove);      
+    });
+  }, []);
+
   return (
     <div className="content-container">
       <h1>Video Speed Controller UI</h1>
