999821306b88143e852d2c419d2bef696f14dd9f
Ben Rose <benrose207@gmail.com>
2021-01-21 11:57:38 -0800

    Complete basic functionality of js30 day30

--- src/components/javascript30/whack_a_mole.jsx
+++ src/components/javascript30/whack_a_mole.jsx
@@ -1,17 +1,71 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const WhackAMole = () => {
-  useEffect(() => {
+  let lastHole;
+  let timeUp = false;
+  let score = 0;
+  const scoreBoard = useRef();
+
+  function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+
+  function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+
+    if (hole === lastHole) {
+      return randomHole(holes);
+    }
+
+    lastHole = hole;
+    return hole;
+  }
+
+  function peep() {
     const holes = document.querySelectorAll('.hole');
-    const scoreBoard = document.querySelector('.score');
-    const moles = document.querySelectorAll('.mole');    
-  }, []);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+
+    const time = randomTime(200, 1000);
+    setTimeout(() => {
+      hole.classList.remove('up');
+      if (!timeUp) peep();
+    }, time);
+  }
+
+  function startGame() {
+    scoreBoard.current.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => {
+      timeUp = true;
+    }, 10000);
+  }
+
+  useEffect(() => {
+    const moles = document.querySelectorAll('.mole');
+
+    function bonk(e) {
+      if (!e.isTrusted) return;
+      score++;
+      this.classList.remove('up');
+      scoreBoard.current.textContent = score;
+    }
+
+    moles.forEach(mole => mole.addEventListener('click', bonk));
+    
+    return (() => {
+      moles.forEach(mole => mole.removeEventListener('click', bonk));
+    });
+  }, [score]);
 
   return (
     <div className="content-container">
-      <h1>Whack-A-Mole! <span className="score">0</span></h1>
+      <h1>Whack-A-Mole! <span className="score" ref={scoreBoard}>0</span></h1>
 
-      <button onClick="startGame()">Start!</button>
+      <button onClick={startGame}>Start!</button>
 
       <div className="game">
         <div className="hole hole1">
